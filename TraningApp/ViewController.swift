492eab4c890c7459233a928c8bdc8ce90581781a
KazuyukiHiga <w-_-waaa1@KazuyukiMac.local>
2015-06-28 16:10:31 +0900

    Initial Commit

---
+++ TraningApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TraningApp
+//
+//  Created by 比嘉　和之 on 2015/06/28.
+//  Copyright (c) 2015年 比嘉　和之. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
